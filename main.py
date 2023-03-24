e4d5d03203ed3930290378f194f598f2e5e6c897
gb9951 <gb9951@g.rit.edu>
2023-03-24 11:34:29 -0400

    Pushing recent changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import os
 import sqlite3
 import time
@@ -47,12 +48,17 @@ def read_input(sql, conn):
 
 
 independent_variables_add = [[]]
+independent_variables_add[0] += ["LAST_LETTER", 'CONTEXT', 'MAXPOSITION',"DIGITS", 'POSITION']
+                                 # "DIGITS", 'POSITION', "DETERMINER", "CONJUNCTION", "FREQUENCY"]
+# 'WORD'
+
+
 for i in range(0, vector_size):
     independent_variables_add[0].append("VEC" + str(i))
-independent_variables_add[0] += ["LAST_LETTER", 'TYPE', 'MAXPOSITION', "DIGITS", 'POSITION']
 # "DETERMINER","CONJUNCTION","FREQUENCY", 'CONTEXT','WORD',
 
 def createFeatures(data):
+    print(data)
     startTime = time.time()
     words = data["WORD"]
     model = createModel(words, "output/gensimTrainingModel.pkl")
@@ -63,7 +69,14 @@ def createFeatures(data):
     createDeterminerFeature(data)
     createConjunctionFeature(data)
     createFrequencyFeature(data)
+    wordLength(data)
     print("Total Feature Time: " + str((time.time() - startTime)))
+
+
+def wordLength(data):
+    words = data["WORD"]
+    wordLengths = [len(word) for word in words]
+    data.insert(0, "WORD_LENGTH", wordLengths)
 
 
 def createFrequencyFeature(data):
@@ -103,7 +116,11 @@ def createLetterFeature(data):
 
 
 def getCosine(vectorA, vectorB):
-    return np.dot(vectorA, vectorB) / (norm(vectorA, axis=0) * norm(vectorB))
+    cosine = np.dot(vectorA, vectorB) / (norm(vectorA, axis=0) * norm(vectorB))
+    if math.isnan(cosine):
+        return 0
+    else:
+        return cosine
 
 
 def createModel(words, pklFile=""):
@@ -113,33 +130,32 @@ def createModel(words, pklFile=""):
     modelGensim = api.load('fasttext-wiki-news-subwords-300')  # CJ, D, P, VM?
     # for word in words[0:20]:
     #     print(word, modelGensim.most_similar(word.lower()))
-    modelWords = list(words) + list(modelGensim.index_to_key)
-
-    # '/Users/gavinburris/gensim-data/conceptnet-numberbatch-17-06-300/conceptnet-numberbatch-17-06-300.gz
-    # '/Users/gavinburris/gensim-data/glove-wiki-gigaword-300/glove-wiki-gigaword-300.gz
-    # '/Users/gavinburris/gensim-data/glove-wiki-gigaword-50/glove-wiki-gigaword-50.gz'
-    # '/Users/gavinburris/gensim-data/glove-twitter-200/glove-twitter-200.gz' Had 1.00 Recall for VM
-    # '/Users/gavinburris/gensim-data/fasttext-wiki-news-subwords-300/fasttext-wiki-news-subwords-300.gz'
-
-    tokenizer = WordPunctTokenizer()
-    words_tok = [tokenizer.tokenize(word.lower()) for word in modelWords]
-    model = Word2Vec(words_tok, vector_size=vector_size, min_count=1, workers=1).wv
-    model.vectors_lockf = np.ones(len(model), dtype=REAL)
-    joblib.dump(model, "output/gensimTrainingModel.pkl")
+    # modelWords = list(words) + list(modelGensim.index_to_key)
+    #
+    # # '/Users/gavinburris/gensim-data/conceptnet-numberbatch-17-06-300/conceptnet-numberbatch-17-06-300.gz
+    # # '/Users/gavinburris/gensim-data/glove-wiki-gigaword-300/glove-wiki-gigaword-300.gz
+    # # '/Users/gavinburris/gensim-data/glove-wiki-gigaword-50/glove-wiki-gigaword-50.gz'
+    # # '/Users/gavinburris/gensim-data/glove-twitter-200/glove-twitter-200.gz' Had 1.00 Recall for VM
+    # # '/Users/gavinburris/gensim-data/fasttext-wiki-news-subwords-300/fasttext-wiki-news-subwords-300.gz'
+    #
+    # tokenizer = WordPunctTokenizer()
+    # words_tok = [tokenizer.tokenize(word.lower()) for word in modelWords]
+    # model = Word2Vec(words_tok, vector_size=vector_size, min_count=1, workers=1).wv
+    # model.vectors_lockf = np.ones(len(model), dtype=REAL)
+    joblib.dump(modelGensim, "output/gensimTrainingModel.pkl")
     # model.save("output/gensimTrainingModel.pkl")
 
     # model.intersect_word2vec_format(
     #     '/Users/gavinburris/gensim-data/conceptnet-numberbatch-17-06-300/conceptnet-numberbatch-17-06-300.gz')
     # for word in words[0:20]:
     #     print(word, model.most_similar(word.lower()))
-    return model
+    return modelGensim
 
 
 def createWordVectorsFeature(model, data):
     words = data["WORD"]
-    zeroVector = [0] * 300
-    # if word in model.index_to_key else zeroVector
-    vectors = [model.get_vector(word.lower()) for word in words]
+    zeroVector = np.zeros_like(model.get_vector("and"))
+    vectors = [model.get_vector(word) if word in model.index_to_key else zeroVector for word in words]
     for i in range(0, vector_size):
         data.insert(i, "VEC" + str(i), np.array([vector[i] for vector in vectors]))
     return vectors
@@ -167,10 +183,14 @@ def createCosinesFeature(model, data, vectors):
                                  posDict[key]]).mean(axis=0)
 
     for i in range(len(words)):
+        print(words[i], getCosine(vectors[i], posDict["CJ"]), getCosine(vectors[i], posDict["D"]), getCosine(vectors[i], posDict["DT"]))
         cosinesCJ.append(getCosine(vectors[i], posDict["CJ"]))
         cosinesD.append(getCosine(vectors[i], posDict["D"]))
         cosinesDT.append(getCosine(vectors[i], posDict["DT"]))
 
+    print("COSINE_CJ", cosinesCJ)
+    print("COSINE_D", cosinesD)
+    print("COSINE_DT", cosinesDT)
     data.insert(0, "COSINE_CJ", np.array(cosinesCJ))
     data.insert(0, "COSINE_D", np.array(cosinesD))
     data.insert(0, "COSINE_DT", np.array(cosinesDT))
@@ -242,19 +262,25 @@ def main():
         print("Process completed in " + str(end - start) + " seconds")
 
 
-def annotate_word(normalized_length, code_context, last_letter, word_type, max_position, digits, position):
-    input_model = 'model_DecisionTreeClassifier.pkl'
+def annotate_word(normalized_length, code_context, last_letter, max_position, digits, position,
+                  determiner, conjunction, frequency, vectors):
+    input_model = 'output/model_DecisionTreeClassifier.pkl'
 
     data = {'NORMALIZED_POSITION': [normalized_length],
             'LAST_LETTER': [last_letter],
-            'TYPE': [word_type],
             'MAXPOSITION': [max_position],
             'DIGITS': [digits],
-            'POSITION': [position]
+            'POSITION': [position],
+            'CONTEXT': [code_context],
+            # 'DETERMINER': [determiner],
+            # 'CONJUNCTION': [conjunction],
+            # 'FREQUENCY': [frequency]
             }
+    for i, vector in enumerate(vectors):
+        data["VEC" + str(i)] = vector
+
     df_features = pd.DataFrame(data,
-                               columns=['NORMALIZED_POSITION', "LAST_LETTER", 'TYPE', 'MAXPOSITION', "DIGITS",
-                                        'POSITION'])
+                               columns=independent_variables_base + independent_variables_add[0])
     # df_features = pd.DataFrame(data,
     #                            columns=['SWUM_TAG', 'POSSE_TAG', 'STANFORD_TAG', 'FLAIR_TAG', 'NORMALIZED_POSITION', 'CONTEXT'])
 
@@ -272,12 +298,14 @@ def read_from_database():
     connection = sqlite3.connect(input_file)
 
     df_input = pd.read_sql_query(sql_statement, connection)
+    outputFile = "output/model_DecisionTreeClassifier_predictions.csv"
     print(" --  --  --  -- Read " + str(len(df_input)) + " input rows --  --  --  -- ")
     print("IDENTIFIER,GRAMMAR_PATTERN,WORD,SWUM,STANFORD,CORRECT,PREDICTION,MATCH,SYSTEM,CONTEXT,IDENT",
-          file=open("output/model_DecisionTreeClassifier_predictions.csv", "a"))
+          file=open(outputFile, "a"))
     createFeatures(df_input)
     for i, row in df_input.iterrows():
         print(i)
+        print(row)
         actual_word = row['WORD']
         actual_identifier = row['IDENTIFIER']
         actual_pattern = row['GRAMMAR_PATTERN']
@@ -287,20 +315,28 @@ def read_from_database():
         system = row['SYSTEM']
         ident = row['IDENTIFIER_CODE']
         last_letter = row['LAST_LETTER']
-        word_type = row['TYPE']
+        # word_type = row['TYPE']
         max_position = row['MAXPOSITION']
         digits = row['DIGITS']
         position = row['POSITION']
-        result = annotate_word(normalized_length, code_context, last_letter, word_type, max_position, digits, position)
+        determiner = row["DETERMINER"]
+        conjunction = row["CONJUNCTION"]
+        frequency = row["FREQUENCY"]
+        vectors = []
+        for i in range(vector_size):
+            vectors.append(row["VEC" + str(i)])
+
+        result = annotate_word(normalized_length, code_context, last_letter, max_position, digits, position,
+                               determiner, conjunction, frequency, vectors)
         print(
-            "{identifier},{pattern},{word},{swum},{stanford},{correct},{prediction},{agreement},{system_name},{context},{ident}"
+            "{identifier},{pattern},{word},{correct},{prediction},{agreement},{system_name},{context},{ident}"
             .format(identifier=actual_identifier, word=actual_word, pattern=actual_pattern,
                     correct=correct_tag, prediction=result, agreement=(correct_tag == result),
                     system_name=system, context=code_context, ident=ident, last_letter=last_letter,
-                    word_type=word_type, max_position=max_position, digits=digits, position=position),
-            file=open("output/model_DecisionTreeClassifier_predictions.csv", "a"))
+                    max_position=max_position, digits=digits, position=position),
+            file=open(outputFile, "a"))
 
 
 if __name__ == "__main__":
-    read_from_database()
-    # main()
+    # read_from_database()
+    main()
