8617be37598c1c4016e3491cf911781435e447b6
cnewman <cnewman@kent.edu>
2023-07-19 19:09:59 -0400

    Remove unneeded files

--- powerset.py
+++
@@ -1,6 +0,0 @@
-def powerset(s):
-    x = len(s)
-    for i in range(1 << x):
-        print([s[j] for j in range(x) if (i & (1 << j))])
-#['CONTEXT', 'WORD', 'TYPE', 'NORMALIZED_POSITION', 'POSITION', 'MAXPOSITION']
-powerset(['TYPE', 'PARAMETERS', 'DECLARATIONS', 'LINES', 'RETURNSIZE', 'PARAMAGGLOMSIZE', 'SURROUNDINGSIZE'])
