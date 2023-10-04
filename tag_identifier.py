324210a332218212d0b332f3ff84c188166a754b
cnewman <cnewman@kent.edu>
2023-10-04 17:03:32 -0400

    Continue working on getting features integrated with tagging functionality

--- tag_identifier.py
+++ tag_identifier.py
@@ -10,20 +10,34 @@ print("Loading word vectors!!")
 modelTokens, modelMethods, modelGensimEnglish = createModel()
 print("Word vectors loaded!!")
 
+def context_to_number(context):
+    if context == "ATTRIBUTE":
+        return 1
+    elif context == "CLASS":
+        return 2
+    elif context == "DECLARATION":
+        return 3
+    elif context == "FUNCTION":
+        return 4 
+    elif context == "PARAMETER":
+        return 5
 
 @app.route('/<identifier_type>/<identifier_name>/<identifier_context>')
 def listen(identifier_type, identifier_name, identifier_context):
     root_logger.info("INPUT: {ident_type} {ident_name} {ident_context}".format(ident_type=identifier_type, ident_name=identifier_name, ident_context=identifier_context))
     
     words = ronin.split(identifier_name)
-    max_pos = [len(words) for _ in words]
+    max_pos = [len(words) for word in words]
+    context_type = [identifier_context for _ in range(len(words))]
     normalized_pos = [0 if i == 0 else (2 if i == len(words) - 1 else 1) for i in range(len(words))]
     word_data = pd.DataFrame(words, columns=['WORD'])
-    pos_data = pd.DataFrame(max_pos, columns=['MAXPOS'])
+    pos_data = pd.DataFrame(max_pos, columns=['MAXPOSITION'])
     normalized_data = pd.DataFrame(normalized_pos, columns=['NORMALIZED_POSITION'])
-
-    data = pd.concat([word_data, pos_data, normalized_data], axis=1)
-    print(data)
+    context_data = pd.DataFrame(context_type, columns=['CONTEXT'])
+    
+    context_data['CONTEXT'] = context_data['CONTEXT'].apply(context_to_number)
+    data = pd.concat([word_data, pos_data, normalized_data, context_data], axis=1)
+    
     data = createVerbVectorFeature(data, modelGensimEnglish)
     data = createDeterminerVectorFeature(data, modelGensimEnglish)
     data = createConjunctionVectorFeature(data, modelGensimEnglish)
@@ -33,6 +47,10 @@ def listen(identifier_type, identifier_name, identifier_context):
     data = createPreambleVectorFeature("ENGLISH", data, modelGensimEnglish)
     data = createLetterFeature(data)
     data = wordPosTag(data)
+    
+    data['NLTK_POS'] = data['NLTK_POS'].astype('category')
+    data['NLTK_POS'] = data['NLTK_POS'].cat.codes
+
     data = createSimilarityToVerbFeature("METHODV", modelMethods, data)
     data = createSimilarityToVerbFeature("ENGLISHV", modelGensimEnglish, data)
     data = createSimilarityToNounFeature("METHODN", modelMethods, data)
@@ -59,9 +77,9 @@ class MSG_COLORS:
     UNDERLINE = '\033[4m'
 
 def annotate_word(clf, data):
-    independent_variables_add = ['NORMALIZED_POSITION', "LAST_LETTER", 'CONTEXT', 'MAXPOSITION', 'NLTK_POS', 'POSITION', 'VERB_SCORE', 'DET_SCORE', 'PREP_SCORE', 'CONJ_SCORE', 'PREPOSITION', 'DETERMINER', 'ENGLISHV_SCORE', 'ENGLISHN_SCORE','METHODN_SCORE', 'METHODV_SCORE', 'CODEPRE_SCORE', 'METHODPRE_SCORE', 'ENGLISHPRE_SCORE']
+    independent_variables_add = ['NORMALIZED_POSITION', 'LAST_LETTER', 'CONTEXT', 'MAXPOSITION', 'NLTK_POS', 'POSITION', 'VERB_SCORE', 'DET_SCORE', 'PREP_SCORE', 'CONJ_SCORE', 'PREPOSITION', 'DETERMINER', 'ENGLISHV_SCORE', 'ENGLISHN_SCORE','METHODN_SCORE', 'METHODV_SCORE', 'CODEPRE_SCORE', 'METHODPRE_SCORE', 'ENGLISHPRE_SCORE']
     df_features = pd.DataFrame(data, columns=independent_variables_add)
-
+    print(data)
     y_pred = clf.predict(df_features)
     return y_pred[0]
 
