d4806bf106d52456987152a3ab39b75c1b8f366d
cnewman <cnewman@kent.edu>
2023-10-05 16:30:57 -0400

    Server almost complete

--- main.py
+++ main.py
@@ -1,43 +1,10 @@
-import os, sqlite3, classifier_multiclass, random, nltk
+import os, sqlite3, classifier_multiclass, random, nltk, argparse
 from datetime import datetime
 import classifier_multiclass
 from feature_generator import *
 import pandas as pd
 import numpy as np
-
-# import classifier_training_set_generator
-nltk.download('universal_tagset')
-
-input_file = 'input/det_conj_db2.db'
-sql_statement = 'select * from base'
-# sql_statement = 'select * from training_set_conj_other order by random()';
-# sql_statement = 'select * from training_set_norm order by random()';
-# sql_statement = 'select * from training_set_norm_other order by random()';
-identifier_column = "ID"
-# independent_variables = ['WORD', 'POSITION', 'MAXPOSITION', 'NORMALIZED_POSITION', 'CONTEXT']
-# independent_variables = ['TYPE', 'WORD', 'SWUM_TAG', 'POSSE_TAG', 'STANFORD_TAG', 'NORMALIZED_POSITION', 'CONTEXT']
-independent_variables_base = ['NORMALIZED_POSITION']
-dependent_variable = 'CORRECT_TAG'
-vector_size = 128
-#vector_size_e = 300
-
-# Training Seed: 2797879, 532479
-# Classifier seed: 1271197, 948572
-
-#db2
-# Training Seed: 2227339
-# Classifier Seed: 3801578
-# SEED: 1340345
-
-seed = 1340345
-print("SEED: " + str(seed))
-trainingSeed = 2227339
-classifierSeed = 3801578
-np.random.seed(1129175)
-random.seed(seed)
-
-independent_variables_add = [[]]
-independent_variables_add[0] += ["LAST_LETTER", 'CONTEXT', 'MAXPOSITION', 'NLTK_POS', 'POSITION', 'VERB_SCORE', 'DET_SCORE', 'PREP_SCORE', 'CONJ_SCORE', 'PREPOSITION', 'DETERMINER', 'ENGLISHV_SCORE', 'ENGLISHN_SCORE','METHODN_SCORE', 'METHODV_SCORE', 'CODEPRE_SCORE', 'METHODPRE_SCORE', 'ENGLISHPRE_SCORE'] #  'FIRST_WORD_LENGTH', 'FIRST_WORD_CAPS' 'CONJUNCTION', 'DIGITS'
+from tag_identifier import start_server
 
 def read_input(sql, conn):
     input_data = pd.read_sql_query(sql, conn)
@@ -51,7 +18,23 @@ def read_input(sql, conn):
     input_data = createFeatures(shuffled_input_data)
     return input_data
 
-def main():
+def train():
+    # import classifier_training_set_generator
+    nltk.download('universal_tagset')
+    input_file = 'input/det_conj_db2.db'
+    sql_statement = 'select * from base'
+    identifier_column = "ID"
+    independent_variables_base = ['NORMALIZED_POSITION']
+    dependent_variable = 'CORRECT_TAG'
+    vector_size = 128
+    seed = 1340345
+    trainingSeed = 2227339
+    classifierSeed = 3801578
+    np.random.seed(1129175)
+    random.seed(seed)
+
+    independent_variables_add = [[]]
+    independent_variables_add[0] += ["LAST_LETTER", 'CONTEXT', 'MAXPOSITION', 'NLTK_POS', 'POSITION', 'VERB_SCORE', 'DET_SCORE', 'PREP_SCORE', 'CONJ_SCORE', 'PREPOSITION', 'DETERMINER', 'ENGLISHV_SCORE', 'ENGLISHN_SCORE','METHODN_SCORE', 'METHODV_SCORE', 'CODEPRE_SCORE', 'METHODPRE_SCORE', 'ENGLISHPRE_SCORE']
     count = 0
     for feature_list in independent_variables_add:
         count = count + 1
@@ -116,95 +99,20 @@ def main():
         end = time.time()
         print("Process completed in " + str(end - start) + " seconds")
 
-
-############CURRENTLY NOT EXECUTED###############
-
-def read_from_database():
-    input_file = 'input/revision_testing_db.db'
-    sql_statement = "select * from testing_set_cp_minor"
-    # sql_statement = "select * from testing_set_ca_minor"
-    # sql_statement = "select * from testing_set_np_minor"
-    # sql_statement = "select * from testing_set_na_minor"
-    connection = sqlite3.connect(input_file)
-
-    df_input = pd.read_sql_query(sql_statement, connection)
-    outputFile = "output/model_DecisionTreeClassifier_predictions.csv"
-    print(" --  --  --  -- Read " + str(len(df_input)) + " input rows --  --  --  -- ")
-    print("IDENTIFIER,GRAMMAR_PATTERN,WORD,SWUM,STANFORD,CORRECT,PREDICTION,MATCH,SYSTEM,CONTEXT,IDENT",
-          file=open(outputFile, "a"))
-    df_input = createFeatures(df_input)
-    
-    category_variables = []
-    if 'NLTK_POS' in df_input:
-        category_variables.append('NLTK_POS')
-        df_input['NLTK_POS'] = df_input['NLTK_POS'].astype(str)
-    
-    for category_column in category_variables:
-        if category_column in df_input.columns:
-            df_input[category_column] = df_input[category_column].astype('category')
-            d = dict(enumerate(df_input[category_column].cat.categories))
-            df_input[category_column] = df_input[category_column].cat.codes
-    
-    results_list = []
-    start = time.time()
-    for i, row in df_input.iterrows():
-        actual_word = row['WORD']
-        actual_identifier = row['IDENTIFIER']
-        actual_pattern = row['GRAMMAR_PATTERN']
-        
-        params = {
-            'normalized_length': row['NORMALIZED_POSITION'],
-            'code_context': row['CONTEXT'],
-            'last_letter': row['LAST_LETTER'],
-            'max_position': row['MAXPOSITION'],
-            'position': row['POSITION'],
-            'determiner': row['DETERMINER'],
-            'nltk_pos' : row['NLTK_POS'],
-            #'conjunction': row['CONJUNCTION'],
-            'verb_score': row['VERB_SCORE'],
-            'det_score': row['DET_SCORE'],
-            'prep_score': row['PREP_SCORE'],
-            'conj_score': row['CONJ_SCORE'],
-            'prep': row['PREPOSITION'],
-            'det': row['DETERMINER'],
-            'englishv_score': row['ENGLISHV_SCORE'],
-            'englishn_score': row['ENGLISHN_SCORE'],
-            'methodn_score': row['METHODN_SCORE'],
-            'methodv_score': row['METHODV_SCORE'],
-            'codepre_score': row['CODEPRE_SCORE'],
-            'methodpre_score': row['METHODPRE_SCORE'],
-            'englishpre_score': row['ENGLISHPRE_SCORE'],
-            'first_word_len': row['FIRST_WORD_LENGTH'],
-            'first_word_caps': row['FIRST_WORD_CAPS']
-        }
-        
-        result = annotate_word(params)
-
-        # Append the results to the results_list
-        results_list.append({
-            'identifier': actual_identifier,
-            'pattern': actual_pattern,
-            'word': actual_word,
-            'correct': row['CORRECT_TAG'],
-            'prediction': result,
-            'agreement': (row['CORRECT_TAG'] == result),
-            'system_name': row['SYSTEM'],
-            'context': row['CONTEXT'],
-            'ident': row['IDENTIFIER_CODE'],
-            'last_letter': row['LAST_LETTER'],
-            'max_position': row['MAXPOSITION'],
-            'position': row['POSITION']
-        })
-
-    end = time.time()
-    print("Process completed in " + str(end - start) + " seconds")
-
-    results_df = pd.DataFrame(results_list)
-    output_file = "output/model_RandomForestClassifier_predictions.csv"
-    results_df.to_csv(output_file, index=False, mode='a')
-
-
-
 if __name__ == "__main__":
-    #read_from_database()
-    main()
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument("-v", "--version", action="store_true", help="print tagger application version")
+  parser.add_argument("-r", "--run", action="store_true", help="run server for part of speech tagging requests") 
+  parser.add_argument("-t", "--train", action="store_true", help="run training set to retrain the model")
+
+  args = parser.parse_args()
+
+  if args.version:
+    print("App version 1.0")
+  elif args.run:
+    start_server()
+  elif args.train:
+    train()
+  else:
+    parser.print_usage()
--- tag_identifier.py
+++ tag_identifier.py
@@ -2,13 +2,28 @@ import logging, os, joblib
 import pandas as pd
 from feature_generator import *
 root_logger = logging.getLogger(__name__)
-from flask import Flask
+from flask import Flask, g
 from spiral import ronin
 app = Flask(__name__)
 
-print("Loading word vectors!!")
-modelTokens, modelMethods, modelGensimEnglish = createModel()
-print("Word vectors loaded!!")
+class ModelData:
+    def __init__(self, ModelTokens, ModelMethods,ModelGensimEnglish) -> None:
+        self.ModelTokens = ModelTokens
+        self.ModelMethods = ModelMethods
+        self.ModelGensimEnglish = ModelGensimEnglish
+
+md = None
+def start_server():
+    print("Loading word vectors!!")
+    ModelTokens, ModelMethods, ModelGensimEnglish = createModel()
+    print("Word vectors loaded!!")
+
+    print("Starting server!!")
+    app.run(host='0.0.0.0')
+    print("server started!!")
+
+    global md 
+    md = ModelData(ModelTokens, ModelMethods, ModelGensimEnglish)
 
 def context_to_number(context):
     if context == "ATTRIBUTE":
@@ -28,33 +43,36 @@ def listen(identifier_type, identifier_name, identifier_context):
     
     words = ronin.split(identifier_name)
     max_pos = [len(words) for word in words]
+    position = [pos for pos in range(len(words))]
     context_type = [identifier_context for _ in range(len(words))]
     normalized_pos = [0 if i == 0 else (2 if i == len(words) - 1 else 1) for i in range(len(words))]
+    
     word_data = pd.DataFrame(words, columns=['WORD'])
     pos_data = pd.DataFrame(max_pos, columns=['MAXPOSITION'])
     normalized_data = pd.DataFrame(normalized_pos, columns=['NORMALIZED_POSITION'])
+    position_data = pd.DataFrame(position, columns=['POSITION'])
     context_data = pd.DataFrame(context_type, columns=['CONTEXT'])
     
     context_data['CONTEXT'] = context_data['CONTEXT'].apply(context_to_number)
-    data = pd.concat([word_data, pos_data, normalized_data, context_data], axis=1)
+    data = pd.concat([word_data, pos_data, normalized_data, context_data, position_data], axis=1)
     
-    data = createVerbVectorFeature(data, modelGensimEnglish)
-    data = createDeterminerVectorFeature(data, modelGensimEnglish)
-    data = createConjunctionVectorFeature(data, modelGensimEnglish)
-    data = createPrepositionVectorFeature(data, modelGensimEnglish)
-    data = createPreambleVectorFeature("CODE", data, modelTokens)
-    data = createPreambleVectorFeature("METHOD", data, modelMethods)
-    data = createPreambleVectorFeature("ENGLISH", data, modelGensimEnglish)
+    data = createVerbVectorFeature(data, md.ModelGensimEnglish)
+    data = createDeterminerVectorFeature(data, md.ModelGensimEnglish)
+    data = createConjunctionVectorFeature(data, md.ModelGensimEnglish)
+    data = createPrepositionVectorFeature(data, md.ModelGensimEnglish)
+    data = createPreambleVectorFeature("CODE", data, md.ModelTokens)
+    data = createPreambleVectorFeature("METHOD", data, md.ModelMethods)
+    data = createPreambleVectorFeature("ENGLISH", data, md.ModelGensimEnglish)
     data = createLetterFeature(data)
     data = wordPosTag(data)
     
     data['NLTK_POS'] = data['NLTK_POS'].astype('category')
     data['NLTK_POS'] = data['NLTK_POS'].cat.codes
 
-    data = createSimilarityToVerbFeature("METHODV", modelMethods, data)
-    data = createSimilarityToVerbFeature("ENGLISHV", modelGensimEnglish, data)
-    data = createSimilarityToNounFeature("METHODN", modelMethods, data)
-    data = createSimilarityToNounFeature("ENGLISHN", modelGensimEnglish, data)
+    data = createSimilarityToVerbFeature("METHODV", md.ModelMethods, data)
+    data = createSimilarityToVerbFeature("ENGLISHV", md.ModelGensimEnglish, data)
+    data = createSimilarityToNounFeature("METHODN", md.ModelMethods, data)
+    data = createSimilarityToNounFeature("ENGLISHN", md.ModelGensimEnglish, data)
     data = createDeterminerFeature(data)
     data = createDigitFeature(data)
     data = createPrepositionFeature(data)
@@ -62,7 +80,7 @@ def listen(identifier_type, identifier_name, identifier_context):
     input_model = 'output/model_RandomForestClassifier.pkl'
     clf = joblib.load(input_model)
 
-    annotate_word(clf, data)
+    return annotate_identifier(clf, data)
     
 
 class MSG_COLORS:
@@ -76,12 +94,8 @@ class MSG_COLORS:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def annotate_word(clf, data):
+def annotate_identifier(clf, data):
     independent_variables_add = ['NORMALIZED_POSITION', 'LAST_LETTER', 'CONTEXT', 'MAXPOSITION', 'NLTK_POS', 'POSITION', 'VERB_SCORE', 'DET_SCORE', 'PREP_SCORE', 'CONJ_SCORE', 'PREPOSITION', 'DETERMINER', 'ENGLISHV_SCORE', 'ENGLISHN_SCORE','METHODN_SCORE', 'METHODV_SCORE', 'CODEPRE_SCORE', 'METHODPRE_SCORE', 'ENGLISHPRE_SCORE']
     df_features = pd.DataFrame(data, columns=independent_variables_add)
-    print(data)
     y_pred = clf.predict(df_features)
-    return y_pred[0]
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    return y_pred
