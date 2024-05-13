ed3518698ab614ce74e07295c43be225e9196e82
Brandon Scholten <scholtenb8890@mtc.edu>
2024-05-12 21:44:59 -0400

    small changes to tests

--- scripts/test_waitress.py
+++ scripts/test_waitress.py
@@ -1,14 +1,23 @@
 import threading
 import requests
 
+host = input('hostname? [0.0.0.0]: ')
+port = input('port? [5000]: ')
+
 def req_one():
-    requests.get(url = 'http://127.0.0.1:5000/numberArray/DECLARATION', params = {})
-    
+    if host == '' or port == '': 
+        print(requests.get(url = 'http://127.0.0.1:5000/numberArray/DECLARATION', params = {}))
+    else: print(requests.get(url = 'https://'+host+':'+port+'/numberArray/DECLARATION', params = {}))
+
 def req_two():
-    requests.get(url = 'http://127.0.0.1:5000/GetNumberArray/FUNCTION', params = {})
+    if host == '' or port == '': 
+        print(requests.get(url = 'http://127.0.0.1:5000/GetNumberArray/FUNCTION', params = {}))
+    else: print(requests.get(url = 'https://'+host+':'+port+'/GetNumberArray/FUNCTION', params = {}))
     
 def req_three():
-    requests.get(url = 'http://127.0.0.1:5000/PersonRecord/CLASS', params = {})
+    if host == '' or port == '': 
+        requests.get(url = 'http://127.0.0.1:5000/PersonRecord/CLASS', params = {})
+    else: print(requests.get(url = 'https://'+host+':'+port+'/PersonRecord/CLASS', params = {}))
 
 r1 = threading.Thread(target=req_one, args=())
 r2 = threading.Thread(target=req_two, args=())
