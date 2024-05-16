ced6de9f1fbc29ad394b001760fb0bef00852570
brandonscholten <scholtenb8890@mtc.edu>
2024-05-16 17:35:37 -0400

    changed test

--- scripts/test_waitress.py
+++ scripts/test_waitress.py
@@ -6,18 +6,18 @@ port = input('port? [5000]: ')
 
 def req_one():
     if host == '' or port == '': 
-        print(requests.get(url = 'http://127.0.0.1:5000/numberArray/DECLARATION', params = {}))
-    else: print(requests.get(url = 'https://'+host+':'+port+'/numberArray/DECLARATION', params = {}))
+        print(requests.get(url = 'http://127.0.0.1:5000/numberArray/DECLARATION', params = {}).text)
+    else: print(requests.get(url = 'https://'+host+':'+port+'/numberArray/DECLARATION', params = {}).text)
 
 def req_two():
     if host == '' or port == '': 
-        print(requests.get(url = 'http://127.0.0.1:5000/GetNumberArray/FUNCTION', params = {}))
-    else: print(requests.get(url = 'https://'+host+':'+port+'/GetNumberArray/FUNCTION', params = {}))
+        print(requests.get(url = 'http://127.0.0.1:5000/GetNumberArray/FUNCTION', params = {}).text)
+    else: print(requests.get(url = 'https://'+host+':'+port+'/GetNumberArray/FUNCTION', params = {}).text)
     
 def req_three():
     if host == '' or port == '': 
-        requests.get(url = 'http://127.0.0.1:5000/PersonRecord/CLASS', params = {})
-    else: print(requests.get(url = 'https://'+host+':'+port+'/PersonRecord/CLASS', params = {}))
+        requests.get(url = 'http://127.0.0.1:5000/PersonRecord/CLASS', params = {}).text
+    else: print(requests.get(url = 'https://'+host+':'+port+'/PersonRecord/CLASS', params = {}).text)
 
 r1 = threading.Thread(target=req_one, args=())
 r2 = threading.Thread(target=req_two, args=())
