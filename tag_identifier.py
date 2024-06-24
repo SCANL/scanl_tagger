d1f7b06ec393c8d45266e1ff5bf8ca23556db1d3
user <scholtenb8890@mtc.edu>
2024-06-24 11:31:11 -0400

    Add version information

--- tag_identifier.py
+++ tag_identifier.py
@@ -47,11 +47,13 @@ class AppCache:
         info.update({"firstEncounter": time.time()})
         info.update({"lastEncounter": time.time()})
         info.update({"count": 1})
+        info.update({"version": "SCANL 1.0"})
         self.Cache.update({identifier : info})
 
     def encounter(self, identifier):
         self.Cache[identifier].update({"lastEncounter": time.time()})
         self.Cache[identifier].update({"count": self.Cache[identifier]["count"]+1})
+        self.Cache[identifier].update({"verion": "SCANL 1.0"})
 
     def save(self):
         JSONcache = open(self.Path+"/cache.json", 'w')
