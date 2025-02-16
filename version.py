498005865551e1a5f2d63c944b95c757f3eda6b5
Christian Newman <cdvnse@rit.edu>
2025-02-15 21:09:37 -0500

    Bump version

--- version.py
+++ version.py
@@ -1,2 +1,2 @@
-__version__ = "2.0.0"  # Changed to match docstring version
+__version__ = "2.1.0"  # Changed to match docstring version
 __version_info__ = tuple(int(num) for num in __version__.split("."))
